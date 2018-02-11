1443318d3bd85cae80aad9535320b9f34946efe0
sohankervin <sohankervin@gmail.com>
2018-02-10 21:50:10 -0700

    Create run_button.py
    this is the source code for the undo_button

---
+++ run_button.py
@@ -0,0 +1,19 @@
+#! /usr/bin/python
+# run_button.py
+
+""" this is to run the undo_button function, and show that undo_button works. """
+from undo_button import Counter
+
+""" run the functions from undo_button """
+tally = Counter()
+tally.reset()
+
+tally.click()
+tally.click()
+
+tally.undo_button()
+
+result = tally.getValue()
+print "Result: " + str(result)
+
+tally.undo_button()
